1d4d0d113e2d650c2eccf244203389771b2ee354
eadventurous <e.o.ivkov@gmail.com>
2019-12-26 16:32:36 +0100

    boilerplate

---
+++ sample-game/src/index.ts
@@ -0,0 +1,3 @@
+import Game from "../../engine/Game";
+
+let game = new Game();
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./sample-game/src/index.ts",
+    output: {
+        path: path.resolve(__dirname, 'sample-game/dist'),
+        filename: "bundle.js",
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+            { test: /\.tsx?$/, use: ["ts-loader"], exclude: /node_modules/ }
+        ]
+    }
+}
