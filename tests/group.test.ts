680b25049f19608e653204e1f24ccb8183e806a0
eadventurous <e.o.ivkov@gmail.com>
2020-01-23 16:21:50 +0300

    corrections for entity constructors

--- tests/group.test.ts
+++ tests/group.test.ts
@@ -7,17 +7,17 @@ class TestComponent extends Component { }
 class TestComponent1 extends Component { }
 
 test("matching entities on group create", () => {
-    const entity1 = new Entity(() => { }, () => { }).addComponent(TestComponent);
-    const entity2 = new Entity(() => { }, () => { }).addComponent(TestComponent1);
-    const entity3 = new Entity(() => { }, () => { });
+    const entity1 = new Entity(() => { }, () => { }, () => { }).addComponent(TestComponent);
+    const entity2 = new Entity(() => { }, () => { }, () => { }).addComponent(TestComponent1);
+    const entity3 = new Entity(() => { }, () => { }, () => { });
     const group = new Group([TestComponent], [entity1, entity2, entity3]);
     expect(group.matchingEntities).toEqual([entity1]);
 });
 
 test("mathing entities after group update", () => {
-    const entity1 = new Entity(() => { }, () => { }).addComponent(TestComponent);
-    const entity2 = new Entity(() => { }, () => { }).addComponent(TestComponent1);
-    const entity3 = new Entity(() => { }, () => { });
+    const entity1 = new Entity(() => { }, () => { }, () => { }).addComponent(TestComponent);
+    const entity2 = new Entity(() => { }, () => { }, () => { }).addComponent(TestComponent1);
+    const entity3 = new Entity(() => { }, () => { }, () => { });
     const group = new Group([TestComponent], [entity1, entity2, entity3]);
     entity3.addComponent(TestComponent);
     group.update(entity3, ComponentEvent.Added);
