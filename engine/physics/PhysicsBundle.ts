1abfe666dae48f62031023e6c5226e0287b84ce6
Egor Ivkov <e.o.ivkov@gmail.com>
2020-02-27 18:27:27 +0300

    corrected physics bundle to include collision system

--- engine/physics/PhysicsBundle.ts
+++ engine/physics/PhysicsBundle.ts
@@ -1,6 +1,10 @@
 import { SystemBundle } from "../ecs-core/SystemBundle";
 import KinematicSystem from "./KinematicSystem";
+import SimpleCollisionSystem from "./SimpleCollisionSystem";
 
+/**
+ * Add this bundle to the world to get all the esential physics systems. Or if you want smaller build size, select by yourself what to add.
+ */
 export default function getRendererBundle(): SystemBundle {
-    return () => [[], [KinematicSystem]];
+    return () => [[], [KinematicSystem, SimpleCollisionSystem]];
 }
