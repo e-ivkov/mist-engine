1b903b7b70b60811f05d1bbfad8dc704ed5525be
Egor Ivkov <e.o.ivkov@gmail.com>
2020-02-27 13:23:35 +0300

    added docs to execute system

--- engine/ecs-core/ExecuteSystem.ts
+++ engine/ecs-core/ExecuteSystem.ts
@@ -7,6 +7,11 @@ import Component from "./Component";
 
 export default abstract class ExecuteSystem extends System {
 
+    /**
+     * 
+     * @param deltaTime time between previous and current frames in milliseconds
+     * @param etities Entities that match awake condition, e.g. have all the components listed there on them. Or undefined if awake condition is always.
+     */
     abstract update(deltaTime: number, etities?: ReadonlyArray<Entity>): void;
 
     /**
